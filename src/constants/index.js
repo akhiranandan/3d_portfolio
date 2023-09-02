9d63f00d12ac3b5b676599921892a392e82050c9
Akhira Nandan <akhiranandanp89548@gmail.com>
2023-09-02 18:52:51 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,246 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    morganchase,
+    microsoft,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    canva,
+    bootstrap
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Student",
+      icon: mobile,
+    },
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    // {
+    //   name: "Redux Toolkit",
+    //   icon: redux,
+    // },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    // {
+    //   name: "MongoDB",
+    //   icon: mongodb,
+    // },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "bootstrap",
+      icon: bootstrap,
+    },
+    {
+      name: "canva",
+      icon: canva,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Web Development Projects",
+      company_name: "javascript, react.js, node.js",
+      icon: reactjs,
+      iconBg: "#383E56",
+      date: "2022 - 2023",
+      points: [
+        "Developed web applications using React.js and other related technologies.",
+        "Made Face Recognition App using Javascript Face API, Virtual Keyboard, Netflix Show Website & few more Projects.",
+        
+      ],
+    },
+    {
+      title: "Intern Mentee",
+      company_name: "Microsoft Engage",
+      icon: microsoft,
+      iconBg: "#383E56",
+      date: "May 4, 2022 - May 27, 2022",
+      points: [
+        "Developed a browser-based application to demonstrate application of Face Recognition technology.",
+        "Got recommendation for the interview process for software engineering intern based on performance in the mentorship project and assignments.",
+        "Cleared the interview round and got an offer for the role of software engineering intern.",
+      ],
+    },
+    {
+      title: "Software Engineering Virtual Experience Program",
+      company_name: "JP Morgan Chase & Co.",
+      icon: morganchase,
+      iconBg: "#E6DEDD",
+      date: "July 2022 - August 2022",
+      points: [
+        "Participated in the open access JP Morgan Chase and Co. Software Engineering Virtual Experience Program. The tasks done are:",
+        "Interface with a stock price data feed",
+        "Use JPMorgan Chase frameworks and tools",
+        "Display data visually for traders",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
